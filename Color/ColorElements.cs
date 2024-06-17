7e85a274f588a6310c9f6d659e45167246d20293
Marvin Marjan <marvin.marjan@gmail.com>
2024-06-17 14:49:45 -0400

    add RGB coloring support.

--- Color/ColorElements.cs
+++ Color/ColorElements.cs
@@ -3,6 +3,13 @@
 namespace Specter.Color;
 
 
+public enum ColorLayer
+{
+	Foreground = 38,
+	Background = 48
+}
+
+
 public class ColorCodeElement(Color16? code = null) : IANSISequenceElement
 {
 	public Color16? code = code;
@@ -16,24 +23,38 @@ public class ColorCodeElement(Color16? code = null) : IANSISequenceElement
 }
 
 
-public class Color256Element(int? code = null, Color256Element.Layer layer = Color256Element.Layer.Foreground) : IANSISequenceElement
+public class Color256Element(byte? code = null, ColorLayer layer = ColorLayer.Foreground) : IANSISequenceElement
 {
-	public enum Layer
-	{
-		Foreground = 38,
-		Background = 48
-	}
-
-
-	public int? code = code;
-	public Layer layer = layer;
+	public byte? code = code;
+	public ColorLayer layer = layer;
 
 	public string BuildSequence()
 	{
 		int layerCode = (int)layer;
 
 		return SequenceBuilder.BuildANSIEscapeSequence([
-			layerCode.ToString(), EscapeCodes.Color256TypeCode, code?.ToString() ?? string.Empty
+			layerCode.ToString(), EscapeCodes.Color256TypeCode, code?.ToString()
+		], false);
+	}
+}
+
+
+public class ColorRGBElement(RGBColor color = new(), ColorLayer layer = ColorLayer.Foreground) : IANSISequenceElement
+{
+	public RGBColor color = color;
+	public ColorLayer layer = layer;
+
+
+	public string BuildSequence()
+	{
+		int layerCode = (int)layer;
+
+		if (!color.AreAllChannelsNull())
+			color.SetValueToNullChannels(0);
+
+		return SequenceBuilder.BuildANSIEscapeSequence([
+			layerCode.ToString(), EscapeCodes.ColorRGBTypeCode,
+			color.r?.ToString(), color.g?.ToString(), color.b?.ToString()
 		], false);
 	}
 }
---
+++ Color/RGBColor.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Specter.Color;
+
+
+public struct RGBColor(byte? r = null, byte? g = null, byte? b = null)
+{
+	public byte? r = r;
+	public byte? g = g;
+	public byte? b = b;
+
+
+	public readonly bool AreAllChannelsNull()
+	{
+		return r is null && g is null && b is null;
+	}
+
+	public void SetValueToNullChannels(byte value)
+	{
+		r ??= value;
+		g ??= value;
+		b ??= value;
+	}
+
+	public void SetAll(byte? value)
+	{
+		r = g = b = value;
+	}
+}
